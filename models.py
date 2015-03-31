b4cb1dd64a1d143fbd7b81e1091830f3670dfe22
Jacob Brouwer <jabrouwer82@gmail.com>
2015-03-31 17:20:12 -0500

    Allows for non-integer countdowns.
    This allows countdowns to build up and add days from time to time.
    This is odd behavior, but I like the slice of randomness it adds.

--- models.py
+++ models.py
@@ -9,5 +9,5 @@ class Manga(ndb.Model):
     frequency = ndb.FloatProperty(default=1.0)
     freq_unit = ndb.StringProperty(default='')
     update = ndb.BooleanProperty(default=True)
-    countdown = ndb.IntegerProperty(default=0)
+    countdown = ndb.FloatProperty(default=0.0)
     manga_updates_url = ndb.StringProperty(default='')
